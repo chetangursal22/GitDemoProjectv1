888c32eb03615d583d96d52939b18ce2c02f5101
chetangursal <145833327+chetangursal@users.noreply.github.com>
2024-12-03 09:43:26 +0530

    First Commit..

---
+++ GitDemoProjectv1/src/MainClass.java
@@ -0,0 +1,25 @@
+
+public class MainClass {
+
+	public MainClass() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String string="hello";
+		if(string=="hello")
+		{
+			System.out.println("hello World");
+		
+		}
+		else {
+			System.out.println("No hello");
+		}
+		
+		
+
+	}
+
+}
